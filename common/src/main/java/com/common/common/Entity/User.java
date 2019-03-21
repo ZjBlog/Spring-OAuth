23a3364c2b17784192269476e1cb3f9cc24e7efe
zj <13552960783@163.com>
2019-03-21 13:46:34 +0800

    公共打包

---
+++ common/src/main/java/com/common/common/Entity/User.java
@@ -0,0 +1,15 @@
+package com.common.common.Entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author : ZJ
+ * @date : 19-3-21 下午12:53
+ */
+@Data
+public class User implements Serializable {
+
+    private Integer id;
+}
